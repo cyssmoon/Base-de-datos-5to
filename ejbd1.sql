403aee51b1d631d42847d41737cc61f6591fd8e5
Luna Estanga <yasestanga@gmail.com>
2026-04-10 03:28:53 -0300

    Update ejbd1.sql
    lo corregi

--- ejbd1.sql
+++ ejbd1.sql
@@ -2,11 +2,13 @@ DROP DATABASE IF EXISTS empresa;
 CREATE DATABASE empresa;
 USE empresa;
 
+
 CREATE TABLE proveedores(
 NIF CHAR(50) PRIMARY KEY,
 nombre CHAR(50),
 direccion CHAR(50)
 );
+
 
 CREATE TABLE productos( 
 codigo INT PRIMARY KEY,
@@ -17,6 +19,8 @@ FOREIGN KEY(NIF_provee) REFERENCES proveedores(NIF)
 );
 
 
+
+
 CREATE TABLE clientes(
 DNI int PRIMARY KEY,
 nombre CHAR(15),
@@ -25,12 +29,16 @@ direccion VARCHAR(15),
 fecha_nac DATE
 );
 
+
 CREATE TABLE compras(
     ID INT, 
     dni_comp INT,
     codigo_prod INT,
     PRIMARY KEY (dni_comp, codigo_prod),
-    FOREIGN KEY (dni_comp) REFERENCES cliente(dni),
+    FOREIGN KEY (dni_comp) REFERENCES clientes(DNI),
     FOREIGN KEY (codigo_prod) REFERENCES productos(codigo)      
-);
-//esta mal compras, tengo q corregirlo en casa mi cerebro se frito en clase 
+); 
+
+
+
+
